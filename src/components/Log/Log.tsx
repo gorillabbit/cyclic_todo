ade8c0ff5ca3aac33c87e52061bf2fe7cd48bfc9
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-02-02 17:15:51 +0900

    チップのピンどめ機能

--- src/components/Log/Log.tsx
+++ src/components/Log/Log.tsx
@@ -1,19 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { LogType, LogsCompleteLogsType } from "../../types";
-import { format, differenceInDays } from "date-fns";
-import { checkLastLogCompleted } from "../../utilities/dateUtilities";
+import { format } from "date-fns";
 import { Box, Typography, Card } from "@mui/material";
 import { BodyTypography } from "../TypographyWrapper";
 import Stopwatch from "./Stopwatch";
 import LogHeader from "./LogHeader";
 import CompleteLog from "./CompleteLog";
-import ChipWrapper from "../ChipWrapper";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconName } from "@fortawesome/fontawesome-svg-core";
 import LogStartButton from "./LogStartButton";
 import LogDeleteButton from "./LogDeleteButton";
 import LogCompleteButton from "./LogCompleteButton";
-import LogDisplayFeatureSelector from "./LogDisplayFeatureSelector";
+import LogFeature from "./LogFeature";
 
 interface LogProps {
   log: LogType;
@@ -22,9 +20,6 @@ interface LogProps {
 
 const Log: React.FC<LogProps> = ({ log, logsCompleteLogs }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  //前回からの経過時間を表示する
-  const [intervalFromLastCompleted, setIntervalFromLastCompleted] =
-    useState<string>("");
 
   const completeLogs = logsCompleteLogs.filter(
     (logsCompleteLog: LogsCompleteLogsType) => logsCompleteLog.logId === log.id
@@ -42,28 +37,6 @@ const Log: React.FC<LogProps> = ({ log, logsCompleteLogs }) => {
 
   //durationが開始しているかどうか
   const isStarted = completeLogs[0]?.type === "start";
-
-  useEffect(() => {
-    if (isLastCompletedAvailable) {
-      const lastLogCompleted = checkLastLogCompleted(lastCompleted) || "0分";
-      setIntervalFromLastCompleted(lastLogCompleted);
-      const timerId = setInterval(() => {
-        setIntervalFromLastCompleted(lastLogCompleted);
-      }, 1000 * 60); // 1分ごとに更新
-      return () => {
-        clearInterval(timerId);
-      };
-    }
-  }, [lastCompleted, isLastCompletedAvailable]);
-  //これまでの完了回数
-  const completedCounts = finishLogs.length;
-  const todayCompletedCounts = finishLogs.filter(
-    (finishLog) =>
-      differenceInDays(
-        new Date(),
-        finishLog.timestamp ? finishLog.timestamp.toDate() : new Date()
-      ) < 1
-  );
 
   return (
     <Box>
@@ -83,23 +56,14 @@ const Log: React.FC<LogProps> = ({ log, logsCompleteLogs }) => {
             visibility={isStarted ? "visible" : "hidden"}
             text={isStarted ? <Stopwatch log={log} /> : <div>blank</div>}
           />
-          {intervalFromLastCompleted && (
-            <ChipWrapper label={"前回から" + intervalFromLastCompleted} />
-          )}
 
-          {log.interval && (
-            <ChipWrapper
-              label={"標準間隔" + log.intervalNum + log.intervalUnit}
-            />
-          )}
-          <ChipWrapper label={"本日" + todayCompletedCounts.length + "回"} />
-          <ChipWrapper label={"通算" + completedCounts + "回"} />
-          {log.availableVoiceAnnounce && (
-            <ChipWrapper
-              label={"音声案内 " + log.voiceAnnounceNum + log.voiceAnnounceUnit}
-            />
-          )}
-          {isOpen && <LogDisplayFeatureSelector log={log} />}
+          <LogFeature
+            log={log}
+            isLastCompletedAvailable={isLastCompletedAvailable}
+            lastCompleted={lastCompleted}
+            finishLogs={finishLogs}
+            isOpen={isOpen}
+          />
 
           {isOpen &&
             completeLogs.map((log: LogsCompleteLogsType) => (
---
+++ src/components/Log/LogFeature.tsx
@@ -0,0 +1,106 @@
+import { updateDocLog } from "../../firebase";
+import { LogType, LogsCompleteLogsType } from "../../types";
+import ChipWrapper from "../ChipWrapper";
+import { differenceInDays } from "date-fns";
+import { useEffect, useState } from "react";
+import { checkLastLogCompleted } from "../../utilities/dateUtilities";
+
+interface LogFeatureProp {
+  log: LogType;
+  isLastCompletedAvailable: boolean;
+  lastCompleted: string;
+  finishLogs: LogsCompleteLogsType[];
+  isOpen: boolean;
+}
+
+const updateDisplayFeature = (log: LogType, feature: string) => {
+  if (log.displayFeature.includes(feature)) {
+    const newDisplayFeature = log.displayFeature.filter(
+      (item: string) => item !== feature
+    );
+    console.log(newDisplayFeature);
+    updateDocLog(log.id, { displayFeature: [...newDisplayFeature] });
+  } else {
+    updateDocLog(log.id, { displayFeature: [...log.displayFeature, feature] });
+  }
+};
+
+const LogFeature: React.FC<LogFeatureProp> = ({
+  log,
+  isLastCompletedAvailable,
+  lastCompleted,
+  finishLogs,
+  isOpen,
+}) => {
+  const [intervalFromLastCompleted, setIntervalFromLastCompleted] =
+    useState<string>("");
+
+  useEffect(() => {
+    if (isLastCompletedAvailable) {
+      const lastLogCompleted = checkLastLogCompleted(lastCompleted) || "0分";
+      setIntervalFromLastCompleted(lastLogCompleted);
+      const timerId = setInterval(() => {
+        setIntervalFromLastCompleted(lastLogCompleted);
+      }, 1000 * 60); // 1分ごとに更新
+      return () => {
+        clearInterval(timerId);
+      };
+    }
+  }, [lastCompleted, isLastCompletedAvailable]);
+  const completedCounts = finishLogs.length;
+  const todayCompletedCounts = finishLogs.filter(
+    (finishLog) =>
+      differenceInDays(
+        new Date(),
+        finishLog.timestamp ? finishLog.timestamp.toDate() : new Date()
+      ) < 1
+  );
+  const displayFeature = log.displayFeature;
+  if (!displayFeature) {
+    return <></>;
+  }
+  const displayFeatureMap = [
+    {
+      title: "前回からの間隔",
+      label: "前回から" + intervalFromLastCompleted,
+      flag: intervalFromLastCompleted,
+    },
+    {
+      title: "標準間隔",
+      label: "標準間隔" + log.intervalNum + log.intervalUnit,
+      flag: log.interval,
+    },
+    {
+      title: "本日回数",
+      label: "本日" + todayCompletedCounts.length + "回",
+      flag: true,
+    },
+    {
+      title: "通算回数",
+      label: "通算" + completedCounts + "回",
+      flag: true,
+    },
+    {
+      title: "音声案内",
+      label: "音声案内 " + log.voiceAnnounceNum + log.voiceAnnounceUnit,
+      flag: log.availableVoiceAnnounce,
+    },
+  ];
+  return (
+    <>
+      {displayFeatureMap.map(
+        (mapItem) =>
+          mapItem.flag &&
+          (displayFeature.includes(mapItem.title) || isOpen) && (
+            <ChipWrapper
+              label={mapItem.label}
+              isSelected={displayFeature.includes(mapItem.title)}
+              onClick={() => updateDisplayFeature(log, mapItem.title)}
+            />
+          )
+      )}
+    </>
+  );
+};
+
+export default LogFeature;
