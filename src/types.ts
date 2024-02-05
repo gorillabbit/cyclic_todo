849b4d7076d6d3ebcda1ae40efaa45cef3115d3c
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-02-05 19:16:52 +0900

    #20 Taskに説明を記入できるようにする

--- src/types.ts
+++ src/types.ts
@@ -10,11 +10,12 @@ export interface TaskType {
   dueTime: string | Date;
   completed: boolean;
   is周期的: string;
-  周期日数?: string;
-  周期単位?: string;
+  周期日数: string;
+  周期単位: string;
   親taskId?: string;
   toggleCompletionTimestamp?: Timestamp;
-  icon?: string;
+  icon: string;
+  description: string;
 }
 export interface LogsCompleteLogsType {
   id?: string;
