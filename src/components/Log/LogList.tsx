d2c30546fc15caff5a3551d9c89775c73e7ab355
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-03-08 17:19:26 +0900

    #45 アーカイブしたログ、タスクは非表示にする

--- src/components/Log/LogList.tsx
+++ src/components/Log/LogList.tsx
@@ -1,10 +1,13 @@
 import { Masonry } from "@mui/lab";
+import { Button } from "@mui/material"; // MUIのButtonをインポート
 import Log from "./Log";
 import { useLog } from "../Context/LogContext";
+import { useState } from "react";
 
 const LogList = () => {
   const { logList, logsCompleteLogsList, sharedLogList } = useLog();
   const mergerLogList = [...logList, ...sharedLogList];
+  const [showArchived, setShowArchived] = useState(false); // archivedログの表示状態を管理
 
   return (
     <>
@@ -31,20 +34,29 @@ const LogList = () => {
             />
           ))}
       </Masonry>
-      <Masonry
-        sx={{ margin: "2px" }}
-        columns={{ xs: 2, sm: 3, md: 4, lg: 5, xl: 6 }}
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setShowArchived(!showArchived)}
       >
-        {mergerLogList
-          .filter((log) => log.archived === true)
-          .map((log) => (
-            <Log
-              log={log}
-              logsCompleteLogs={logsCompleteLogsList}
-              key={log.id}
-            />
-          ))}
-      </Masonry>
+        {showArchived ? "アーカイブログを非表示" : "アーカイブログを表示"}
+      </Button>
+      {showArchived && ( // 状態変数に応じてarchivedログのリストを表示
+        <Masonry
+          sx={{ margin: "2px" }}
+          columns={{ xs: 2, sm: 3, md: 4, lg: 5, xl: 6 }}
+        >
+          {mergerLogList
+            .filter((log) => log.archived === true)
+            .map((log) => (
+              <Log
+                log={log}
+                logsCompleteLogs={logsCompleteLogsList}
+                key={log.id}
+              />
+            ))}
+        </Masonry>
+      )}
     </>
   );
 };
--- src/components/Task/TaskList.tsx
+++ src/components/Task/TaskList.tsx
@@ -1,11 +1,12 @@
 import { Masonry } from "@mui/lab";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import Task from "./Task";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useTask } from "../Context/TaskContext";
 
 const TaskList = () => {
   const { taskList, setTaskList } = useTask();
+  const [showCompleted, setShowCompleted] = useState(false); // Manage the display state of completed tasks
   const uncompletedTaskList = useMemo(
     () => taskList.filter((task) => !task.completed),
     [taskList]
@@ -39,19 +40,28 @@ const TaskList = () => {
           />
         ))}
       </Masonry>
-      <Masonry
-        sx={{ margin: "2px" }}
-        columns={{ xs: 2, sm: 3, md: 4, lg: 5, xl: 6 }}
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setShowCompleted(!showCompleted)}
       >
-        {completedTaskList.map((task) => (
-          <Task
-            key={task.id}
-            task={task}
-            setTaskList={setTaskList}
-            tasklist={taskList}
-          />
-        ))}
-      </Masonry>
+        {showCompleted ? "完了タスクを非表示" : "完了タスクを表示"}
+      </Button>
+      {showCompleted && (
+        <Masonry
+          sx={{ margin: "2px" }}
+          columns={{ xs: 2, sm: 3, md: 4, lg: 5, xl: 6 }}
+        >
+          {completedTaskList.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              setTaskList={setTaskList}
+              tasklist={taskList}
+            />
+          ))}
+        </Masonry>
+      )}
     </Box>
   );
 };
