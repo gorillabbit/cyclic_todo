22470297c64506403d9ea81cfda70ca3921cdc11
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-04-26 07:18:55 +0900

    デフォルトは記録にする

--- src/components/Kakeibo/PurchaseInputs.tsx
+++ src/components/Kakeibo/PurchaseInputs.tsx
@@ -4,7 +4,7 @@ import PurchaseInput from "./PurchaseInput";
 import PurchaseScheduleInput from "./PurchaseScheduleInput";
 
 const InputForms = () => {
-  const [isSchedule, setIsSchedule] = useState<boolean>(true);
+  const [isSchedule, setIsSchedule] = useState<boolean>(false);
   //愚直に三項演算子で条件分岐すると、コンポーネントが再レンダリングされて入力内容が保存されない
   return (
     <Box m={2}>
