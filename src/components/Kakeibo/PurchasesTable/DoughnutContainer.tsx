085492e8a9127f7e1477755cbce3c8286cf52fd1
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-05-21 14:37:39 +0900

    円グラフに金額も示す

--- src/components/Kakeibo/PurchasesTable/DoughnutContainer.tsx
+++ src/components/Kakeibo/PurchasesTable/DoughnutContainer.tsx
@@ -5,32 +5,39 @@ import { PurchaseListType } from "../../../types";
 import {
   isLaterPayment,
   filterPurchasesByIncomeType,
+  sumSpentAndIncome,
 } from "../../../utilities/purchaseUtilities";
 
 type PlainDoughnutContainerProps = {
-  currentMonthIncomeList: PurchaseListType[];
   currentMonthNetSpentList: PurchaseListType[];
+  currentMonthNetSpent: number;
   currentMonthPaymentList: PurchaseListType[];
+  currentMonthPayment: number;
+  currentMonthIncomeList: PurchaseListType[];
+  currentMonthIncome: number;
 };
 
 const PlainDoughnutContainer = memo(
   ({
-    currentMonthIncomeList,
     currentMonthNetSpentList,
+    currentMonthNetSpent,
     currentMonthPaymentList,
+    currentMonthPayment,
+    currentMonthIncomeList,
+    currentMonthIncome,
   }: PlainDoughnutContainerProps) => (
     <Box display="flex" flexWrap="wrap">
       <DoughnutChart
         purchaseList={currentMonthNetSpentList}
-        title="今月の使用金額"
+        title={`今月の使用金額 ${-currentMonthNetSpent}円`}
       />
       <DoughnutChart
         purchaseList={currentMonthPaymentList}
-        title="今月の支払い金額"
+        title={`今月の支払い金額 ${-currentMonthPayment}円`}
       />
       <DoughnutChart
         purchaseList={currentMonthIncomeList}
-        title="今月の収入金額"
+        title={`今月の収入金額 ${currentMonthIncome}円`}
       />
     </Box>
   )
@@ -57,10 +64,18 @@ const DoughnutContainer = ({
   const currentMonthNetSpentList = currentMonthSpentList.filter(
     (spent) => !isLaterPayment(spent)
   );
+  const currentMonthNetSpent = useMemo(
+    () => sumSpentAndIncome(currentMonthNetSpentList),
+    [currentMonthNetSpentList]
+  );
 
   // 今月の支払金額
   const currentMonthPaymentList = currentMonthSpentList.filter(
     (spent) => !spent.childPurchaseId
+  );
+  const currentMonthPayment = useMemo(
+    () => sumSpentAndIncome(currentMonthPaymentList),
+    [currentMonthPaymentList]
   );
 
   const currentMonthIncomeList = useMemo(
@@ -68,10 +83,18 @@ const DoughnutContainer = ({
     [PayLaterCategoryPurchase]
   );
 
+  const currentMonthIncome = useMemo(
+    () => sumSpentAndIncome(currentMonthIncomeList),
+    [currentMonthIncomeList]
+  );
+
   const plainProps = {
-    currentMonthIncomeList,
     currentMonthNetSpentList,
+    currentMonthNetSpent,
     currentMonthPaymentList,
+    currentMonthPayment,
+    currentMonthIncomeList,
+    currentMonthIncome,
   };
 
   return <PlainDoughnutContainer {...plainProps} />;
