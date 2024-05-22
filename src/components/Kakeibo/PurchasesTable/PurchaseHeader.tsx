cb0ea2fbf53e9ee5d4ecfda976009fe95f3085af
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-05-22 11:52:21 +0900

    カテゴリーごとの使用金額を表示する

--- src/components/Kakeibo/PurchasesTable/PurchaseHeader.tsx
+++ src/components/Kakeibo/PurchasesTable/PurchaseHeader.tsx
@@ -11,21 +11,22 @@ import {
 import { memo, useMemo } from "react";
 import { PurchaseListType } from "../../../types";
 import { lastDayOfMonth } from "date-fns";
-import {
-  sumSpentAndIncome,
-  isLaterPayment,
-} from "../../../utilities/purchaseUtilities";
+import { sumSpentAndIncome } from "../../../utilities/purchaseUtilities";
 import { useAsset } from "../../Context/AssetContext";
 
 type PlainPurchaseHeaderProps = {
   currentMoney: number;
   endOfMonthMoneyAmount: number;
+  categorySpent: {
+    [key: string]: number;
+  };
 };
 
 const PlainPurchaseHeader = memo(
   ({
     currentMoney,
     endOfMonthMoneyAmount,
+    categorySpent,
   }: PlainPurchaseHeaderProps): JSX.Element => (
     <>
       <Paper sx={{ marginY: 2 }}>
@@ -47,6 +48,26 @@ const PlainPurchaseHeader = memo(
                 <TableCell>今月末の残高</TableCell>
                 <TableCell>{endOfMonthMoneyAmount}</TableCell>
               </TableRow>
+              <TableRow>
+                <TableCell colSpan={2}>
+                  <Table size="small" sx={{ m: 1 }}>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>カテゴリー名</TableCell>
+                        <TableCell>使用金額</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {Object.entries(categorySpent).map((category) => (
+                        <TableRow key={category[0]}>
+                          <TableCell>{category[0]}</TableCell>
+                          <TableCell>{category[1]}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableCell>
+              </TableRow>
             </TableBody>
           </Table>
         </TableContainer>
@@ -62,12 +83,12 @@ const PurchaseHeader = ({
 }): JSX.Element => {
   const { sumAssets } = useAsset();
   const today = useMemo(() => new Date(), []);
+  const filteredPurchases = purchaseList.filter(
+    (purchase) => purchase.date.toDate() < today
+  );
   const currentMoney = useMemo(() => {
-    const filteredPurchases = purchaseList.filter(
-      (purchase) => purchase.date.toDate() < today && isLaterPayment(purchase)
-    );
     return sumAssets + sumSpentAndIncome(filteredPurchases);
-  }, [purchaseList, sumAssets, today]);
+  }, [filteredPurchases, sumAssets]);
 
   const endOfMonthMoneyAmount = useMemo(() => {
     const filteredPurchases = purchaseList.filter(
@@ -76,9 +97,19 @@ const PurchaseHeader = ({
     return sumAssets + sumSpentAndIncome(filteredPurchases);
   }, [purchaseList, sumAssets, today]);
 
+  const categorySpent: { [key: string]: number } = {};
+  filteredPurchases.forEach((purchase) => {
+    if (categorySpent[purchase.category]) {
+      categorySpent[purchase.category] += Number(purchase.price);
+    } else {
+      categorySpent[purchase.category] = Number(purchase.price);
+    }
+  });
+
   const plainProps = {
     currentMoney,
     endOfMonthMoneyAmount,
+    categorySpent,
   };
   return <PlainPurchaseHeader {...plainProps} />;
 };
