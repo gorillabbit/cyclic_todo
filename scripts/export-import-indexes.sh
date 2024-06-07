2934bf7603348f65354b40130194b0ca5c7f8366
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-06-07 21:13:18 +0900

    parent 8dc157837e8ee14358e2858611c180309b70aaa3 author gorillabbit <modsyoukaizenryoku@gmail.com> 1717762398 +0900 committer gorillabbit <modsyoukaizenryoku@gmail.com> 1717807385 +0900
    firestoreのindexを出力しデプロイする

--- scripts/export-import-indexes.sh
+++ scripts/export-import-indexes.sh
@@ -7,9 +7,3 @@ PROD_PROJECT_ID="cyclictodo"
 
 # インデックスのエクスポート
 firebase firestore:indexes --project $TEST_PROJECT_ID > firestore.indexes.utf16.json
-
-# UTF-16からUTF-8への変換
-iconv -f UTF-16LE -t UTF-8 firestore.indexes.utf16.json firestore.indexes.json -c
-
-# 本番環境にインポート
-firebase firestore:indexes --project $PROD_PROJECT_ID firestore.indexes.json
