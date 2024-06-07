fa0803d5fdd975865abfda7f28d6e7527d202be1
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-06-07 21:13:18 +0900

    fix

--- scripts/export-import-indexes.sh
+++ scripts/export-import-indexes.sh
@@ -9,7 +9,7 @@ PROD_PROJECT_ID="cyclictodo"
 firebase firestore:indexes --project $TEST_PROJECT_ID > firestore.indexes.utf16.json
 
 # UTF-16からUTF-8への変換
-iconv -f UTF-16LE -t UTF-8 firestore.indexes.utf16.json firestore.indexes.json -c
+iconv -f UTF-16LE -t UTF-8 firestore.indexes.utf16.json > firestore.indexes.json 
 
 # 本番環境にインポート
 firebase firestore:indexes --project $PROD_PROJECT_ID firestore.indexes.json
