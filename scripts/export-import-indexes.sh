483fdcc70f683e7f47aa7f296524914234480144
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-06-08 09:59:18 +0900

    不要ファイルを削除

--- scripts/export-import-indexes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# テストプロジェクトID
-TEST_PROJECT_ID="todolist-37a07"
-# 本番プロジェクトID
-PROD_PROJECT_ID="cyclictodo"
-
-# インデックスのエクスポート
-firebase firestore:indexes --project $TEST_PROJECT_ID > firestore.indexes.utf16.json
