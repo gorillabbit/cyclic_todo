31269283b4774bc4b26b7c19775cad5b71ecdb16
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-06-08 09:13:57 +0900

    fix

--- scripts/export-import-indexes.sh
+++ scripts/export-import-indexes.sh
@@ -5,5 +5,5 @@ TEST_PROJECT_ID="todolist-37a07"
 # 本番プロジェクトID
 PROD_PROJECT_ID="cyclictodo"
 
-# 本番環境にインポート
-firebase firestore:indexes --project $PROD_PROJECT_ID firestore.indexes.json
+# インデックスのエクスポート
+firebase firestore:indexes --project $TEST_PROJECT_ID > firestore.indexes.utf16.json
