faf7b88bdb82bd4be2194f511b3c69d6e71e332d
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-06-09 19:23:00 +0900

    #80 本番データを洗い替え

---
+++ scripts/setTabId_2_prd.js
@@ -0,0 +1,50 @@
+const admin = require('firebase-admin');
+
+// サービスアカウントのJSONファイルのパスを指定
+const serviceAccount = require('./service_account_prd.json');
+
+// Firebase Admin SDK の初期化
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();
+
+const taskDbs = [
+  "tasks",
+  "logs",
+  "logsCompleteLogs",
+]
+
+async function updateDocuments(dbName, updateData) {
+  const collectionRef = db.collection(dbName);
+  const snapshot = await collectionRef.where("userId", "==", "RSzGJ1om5iZMMvcmYBgThDhzpjc2").get();
+
+  const batch = db.batch();
+  snapshot.forEach((docSnap) => {
+    const docRef = collectionRef.doc(docSnap.id);
+
+    batch.update(docRef, updateData); // ここに更新したいフィールドと値を指定
+  });
+
+  await batch.commit();
+  console.log('Documents updated successfully.');
+}
+
+taskDbs.forEach((dbName) =>
+  updateDocuments(dbName,  { tabId: 'oYL4pIMfSnPj89SFP5YJ' }).catch(console.error)
+);
+
+const purchaseDbs = [
+    "Purchases",
+    "PurchaseTemplates",
+    "Assets",
+    "PurchaseSchedules",
+    "Methods",
+    "TransferTemplates",
+]
+purchaseDbs.forEach((dbName) =>
+  updateDocuments(dbName,  { tabId: 'oycy7tRzr40Tu3P91hPh' }).catch(console.error)
+);
+
+
+
