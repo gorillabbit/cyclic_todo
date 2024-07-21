2e0966928edf4b415cb33b294ecb1762cea18a74
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-07-21 13:33:23 +0900

    flat config 対応

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,18 @@
+import js from "@eslint/js";
+import parser from "@typescript-eslint/parser";
+import globals from "globals";
 export default [
+  js.configs.recommended,
   {
-    root: true,
-    extends: ["eslint:recommended"],
-    ignorePatterns: ["dist"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["react-refresh"],
-    rules: {
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+    ignores: ["build/**/*"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: parser,
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
   },
 ];
