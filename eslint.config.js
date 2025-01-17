fdeaadfd35ab63dea4115e76c2935464152d887e
gorillabbit <modsyoukaizenryoku@gmail.com>
2025-01-17 13:28:36 +0900

    GitHub ActionsのFirebase Functionsテストプロセスを改善し、ESLint設定にJestプラグインを追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,30 @@
 import js from "@eslint/js";
 import globals from "globals";
+import jestPlugin from "eslint-plugin-jest";
+
 export default [
   js.configs.recommended,
   {
+    plugins: {
+      jest: jestPlugin,
+    },
     ignores: ["build/**/*"],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
+        ...globals.jest,
       },
       ecmaVersion: "latest",
       sourceType: "module",
+    },
+    rules: {
+      "no-cond-assign": ["error", "except-parens"],
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
     },
   },
 ];
